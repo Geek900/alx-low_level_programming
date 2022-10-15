a60200d987a3ca209569f6a2922c64579568dcd8
Geek900 <mkize.zamambo89@gmail.com>
2022-10-14 19:15:25 -0700

    Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	i = 'z';
+	while
+		(i >= 'a') {
+			putchar(i);
+			i--;
+		}
+	putchar('\n');
+	return (0);
+}
