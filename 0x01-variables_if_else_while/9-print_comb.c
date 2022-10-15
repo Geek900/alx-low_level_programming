44ae2cbaee8e8692a558b6b96d6c69b723592081
Geek900 <mkize.zamambo89@gmail.com>
2022-10-14 19:47:52 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		putchar(j + '0');
+		if (j < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
