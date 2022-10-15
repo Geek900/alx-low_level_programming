8ab6edf664fc0c36ad6c5f3a2c44d852058d240f
Geek900 <mkize.zamambo89@gmail.com>
2022-10-14 19:31:03 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	int y;
+
+	x = 'a';
+	y = 0;
+	while
+		(y < 10) {
+			putchar(y + '0');
+			y++;
+		}
+	while
+		(x <= 'f') {
+			putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
