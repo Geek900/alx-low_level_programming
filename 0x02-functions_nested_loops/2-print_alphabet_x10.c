c1f08e22cf0d5ca1a6a5877d01884a6de9250db7
Geek900 <mkize.zamambo89@gmail.com>
2022-10-18 17:17:46 -0700

    times ten

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 	int counter = 0;
 	char alphabet;
 
-	while (counter <= 9; counter++)
+	while (counter++ <= 9)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 			_putchar(alphabet);
