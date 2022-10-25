4a6b1f6942822cedc3a37219ae70631bf3ac12f3
Geek900 <mkize.zamambo89@gmail.com>
2022-10-25 15:19:58 -0700

    one out of two

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of two in a string'
+ * @str: string containing characters
+ */
+void puts2(char *str)
+{
+	int i = 0, length = 0;
+
+	while (str[i++])
+		length++;
+
+	for (i = 0; i < length; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
