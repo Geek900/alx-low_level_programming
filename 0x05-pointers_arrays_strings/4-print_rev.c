8c70191a3b7135345a356c4be0715a9847879583
Geek900 <mkize.zamambo89@gmail.com>
2022-10-25 14:01:53 -0700

    print revese

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string, in reverse,
+ * print_rev - Prints a string in reverse
  * @s: string to be printed
  */
 void print_rev(char *s)
 {
-	int alphabet = 0, index;
+	int j = 0;
 
-	while (s[index++])
-		index++;
-
-	for (index = alphabet - 1; index >= 0; index--)
-		_putchar(s[index]);
+	while (s[i] != '\n')
+		i++;
+	for (i = i - 1; i > 0; i--)
+		_putchar(s[i]);
 
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,9 +1,11 @@
-#include <main.h>
-#include <stdio.h>
+#include <unistd.h>
+
 /**
- * _putchar - writes character c to stdout
- * @c: char to be printed
- * Return: on success 1
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: on sucess 1
+ * on error, -1 is returned, and errorno is set accordingly
  */
 int _putchar(char c)
 {
