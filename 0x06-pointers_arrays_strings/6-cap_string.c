69566aebf77dad6dd9351ba78e60d6d058922dd3
Geek900 <mkize.zamambo89@gmail.com>
2022-10-27 14:20:41 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,22 +13,22 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
 
-		if (str[i - 1] == ' ' || 
-str[i - 1] == '\t' ||
-str[i - 1] == '\n' || 
-str[i - 1] == ',' ||
-str[i - 1] == ';' ||
-str[i - 1] == '.' ||
-str[i - 1] == '!' || 
-str[i - 1] == '?' ||
-str[i - 1] == '"' ||
-str[i - 1] == '(' ||
-str[i - 1] == ')' ||
-str[i - 1] == '{' || 
-str[i - 1] == '}' ||
-i == 0)
+			i++;
+		if (str[i - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}' ||
+				index == 0)
 			str[i] -= 32;
 
 		i++;
